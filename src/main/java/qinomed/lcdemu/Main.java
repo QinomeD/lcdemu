798ec94db44f6db7b897fcac723771b732425f42
QinomeD <31182345+QinomeD@users.noreply.github.com>
2023-11-22 15:16:43 +0100

    having fun xd

--- src/main/java/qinomed/lcdemu/Main.java
+++ src/main/java/qinomed/lcdemu/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame("LCDEmu Test");
-        LCDPanel display = new LCDPanel(window, 20, 9);
+        LCDPanel display = new LCDPanel(window, 40, 18);
         window.add(display);
 
         FontLoader.loadFont("font.txt");
@@ -20,6 +20,11 @@ public class Main {
     }
 
     private static void drawInit(LCDPanel display) {
-        display.drawPixelArray(new int[]{0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0}, 0, 0, 4);
+        /*
+        display.drawPixelArray(new int[]{1,1,1,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,1,1,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1}, 0, 0, 5);
+        display.drawPixelArray(new int[]{0,0,1,0,0,0,1,1,0,0,1,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,1,1,0}, 5, 0, 5);
+        display.drawPixelArray(new int[]{1,1,1,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,1,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,1,1,1,1,0}, 10, 0, 5);
+        display.drawPixelArray(new int[]{1,1,1,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0}, 15, 0, 5);
+        */
     }
 }
